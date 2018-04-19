bfd6b38d960bb20b34594e948e9331c91591da68
Neo.Tian <Neo.Tian@kaiostech.com>
2018-04-19 10:00:57 +0800

    Bug 39716 - SELinux:Add policies for 9820e, r=Seinlin

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -5,6 +5,9 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 BOARD_SEPOLICY_DIRS += gonk-misc/sepolicy/qualcomm
 endif
 ifeq ("$(PRODUCT_MANUFACTURER)", "SPRD")
-BOARD_SEPOLICY_DIRS += gonk-misc/sepolicy/spreadtrum
+BOARD_SEPOLICY_DIRS += gonk-misc/sepolicy/spreadtrum/common
+ifeq ($(strip $(BOARD_TEE_CONFIG)),trusty)
+BOARD_SEPOLICY_DIRS += gonk-misc/sepolicy/spreadtrum/tee
+endif
 endif
 endif
