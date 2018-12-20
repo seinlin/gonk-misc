524fcc8cfcecf64f23d1160f168543d1e14698e8
Seinlin Li <seinlin.li@kaiostech.com>
2018-12-19 23:40:42 -0800

    Bug 49901 - Part 2: Move api-daemon path. r=fabrice

--- api-daemon.sh
+++ api-daemon.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
-
-if [ ! -d /data/local/service ]; then
-  mkdir -p /data/local/service
-  cp -rf /system/kaios/http_root /data/local/service/
-  cp -f /system/kaios/api-daemon /data/local/service/
-  cp -f /system/kaios/config.toml /data/local/service/
+API_DAEMON_DIR=/data/local/service/api-daemon
+if [ ! -d ${API_DAEMON_DIR} ]; then
+  mkdir -p ${API_DAEMON_DIR}
+  cp -rf /system/kaios/http_root ${API_DAEMON_DIR}
+  cp -f /system/kaios/api-daemon ${API_DAEMON_DIR}
+  cp -f /system/kaios/config.toml ${API_DAEMON_DIR}
 fi
 
-exec /data/local/service/api-daemon /data/local/service/config.toml
+exec ${API_DAEMON_DIR}/api-daemon ${API_DAEMON_DIR}/config.toml
