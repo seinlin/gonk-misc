6af7d4ebd6457c2a38fe2ea418433ce80c5c5d33
Cliff Han <cliff.han@kaiostech.com>
2021-02-02 16:25:06 +0800

    Bug 116427 - Copy kota.json to daemon working directory. r=seinlin.li

--- api-daemon.sh
+++ api-daemon.sh
@@ -5,6 +5,7 @@ if [ ! -d ${API_DAEMON_DIR} ]; then
   cp -rf /system/kaios/http_root ${API_DAEMON_DIR}
   cp -f /system/kaios/api-daemon ${API_DAEMON_DIR}
   cp -f /system/kaios/config.toml ${API_DAEMON_DIR}
+  cp -f /system/kaios/kota.json ${API_DAEMON_DIR}
 fi
 
 exec ${API_DAEMON_DIR}/api-daemon ${API_DAEMON_DIR}/config.toml
