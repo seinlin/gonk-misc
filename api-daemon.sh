bd6d3e505aa1c154f557dcdbe9ca1950e6bbf91f
Seinlin Li <seinlin.likaiostech.com>
2021-02-22 15:02:30 -0800

    Bug 117284 - Check the api-daemon version in system and use it if it is newer. r=fabrice.desre

--- api-daemon.sh
+++ api-daemon.sh
@@ -1,6 +1,27 @@
 #!/system/bin/sh
+newver=`cat /system/kaios/api-daemon.ver`
+oldver=`cat /data/local/service/updater/installed/api-daemon-*.toml | grep version | cut -f 2 -d '"'`
+newer=0
+
+IFS="." read -a v_a <<< "$newver"
+IFS="." read -a v_b <<< "$oldver"
+
+i=0
+a=${v_a[$i]}
+b=${v_b[$i]}
+while [[ -n "$a" || -n "$b" ]]; do
+  [[ -z "$a" ]] && a=0
+  [[ -z "$b" ]] && b=0
+  [[ "$a" -gt "$b" ]] && newer=1 && break
+  [[ "$a" -lt "$b" ]] && break
+
+  i=`expr $i + 1`
+  a=${v_a[$i]}
+  b=${v_b[$i]}
+done
+
 API_DAEMON_DIR=/data/local/service/api-daemon
-if [ ! -d ${API_DAEMON_DIR} ]; then
+if [ ! -d ${API_DAEMON_DIR} -o "$newer" = "1" ]; then
   mkdir -p ${API_DAEMON_DIR}
   cp -rf /system/kaios/http_root ${API_DAEMON_DIR}
   cp -f /system/kaios/api-daemon ${API_DAEMON_DIR}
